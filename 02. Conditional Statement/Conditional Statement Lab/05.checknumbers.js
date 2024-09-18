9a106393998ed1bf6bf52dec014bdd6514a70f56
Alper Ahmedov <alper.ahmed@hotmail.com>
2024-09-18 21:40:27 +0300

    new lab files

---
+++ 02. Conditional Statement/Conditional Statement Lab/05.checknumbers.js
@@ -0,0 +1,14 @@
+function checkNumbers(number) {
+
+    if (number < 100) {
+        console.log("Less than 100");
+    } else if (number < 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+        
+    }
+
+}
+
+checkNumbers(120);
---
+++ 02. Conditional Statement/Conditional Statement Lab/06.speedradar.js
@@ -0,0 +1,28 @@
+// При скорост до 10 (включително) отпечатайте "slow"
+// •	При скорост над 10 и до 50 (включително) отпечатайте "average" 
+// •	При скорост над 50 и до 150 (включително) отпечатайте "fast"
+// •	При скорост над 150 и до 1000 (включително) отпечатайте "ultra fast" 
+// •	При по-висока скорост отпечатайте "extremely fast"
+
+
+
+function speedRadar(num) {
+
+    if (num <= 10) {
+        console.log('slow');
+
+    } else if (num <= 50) {
+        console.log('average');
+        
+    } else if (num <=150) {
+        console.log('fast');
+    } else if (num <= 1000) {
+        console.log('ultra fast');
+    } else {
+        console.log('extremly fast');
+        
+    }
+
+}
+
+speedRadar(1800);
